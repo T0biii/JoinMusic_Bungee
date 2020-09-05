ecf48c6dc4cbcaab4d55d1620446114105ff5a00
Tobias <T0biii@users.noreply.github.com>
2020-09-05 12:45:29 +0200

    cleanup imports

--- src/main/java/de/t0biii/joinmusicbungee/JoinMusicBungee.java
+++ src/main/java/de/t0biii/joinmusicbungee/JoinMusicBungee.java
@@ -1,17 +1,6 @@
 package de.t0biii.joinmusicbungee;
 
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.PostLoginEvent;
-import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.event.EventHandler;
-
-import java.util.Collection;
-import java.util.concurrent.TimeUnit;
 
 public final class JoinMusicBungee extends Plugin  {
 
--- src/main/java/de/t0biii/joinmusicbungee/PlayerJoin.java
+++ src/main/java/de/t0biii/joinmusicbungee/PlayerJoin.java
@@ -1,14 +1,10 @@
 package de.t0biii.joinmusicbungee;
 
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 public class PlayerJoin implements Listener {
